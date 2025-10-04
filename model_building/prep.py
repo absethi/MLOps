f04a325d707ba615d76479757df98cb12865f3c1
Abhinav Sethi <42364767+absethi@users.noreply.github.com>
2025-10-05 01:29:17 +0530

    Update prep.py

--- model_building/prep.py
+++ model_building/prep.py
@@ -12,7 +12,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "https://raw.githubusercontent.com/absethi/MLOps/data/tourism.csv"
+DATASET_PATH = "https://raw.githubusercontent.com/absethi/MLOps/main/data/tourism.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
